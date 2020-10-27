85012991cfb7942066165f0d9eb63fc354080ba5
allengu01 <allen.gu01@gmail.com>
2020-10-27 00:09:38 -0500

    Fixed Font Awesome

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -12,7 +12,7 @@ function Navbar() {
                 <Link to="/" className="navbar-logo">
                 </Link>
                 <div className="menu-icon" onClick={handleClick}>
-                    <i class= {click ? "fas fa-times" : "fas fa-bars"}></i>
+                    <i className={click ? "fas fa-times" : "fas fa-bars"}></i>
                 </div>
                 <ul className={click ? 'nav-menu active' : 'nav-menu'}>
                     <li className='nav-item'>
