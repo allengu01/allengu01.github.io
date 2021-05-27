86814600a3c5c62f216e5b640dc7b2ad284b717f
allengu01 <allen.gu01@gmail.com>
2021-05-26 19:51:22 -0500

    Updated projects and began responsiveness

--- src/components/HomeProjectsSection/HomeProjectsSection.js
+++ src/components/HomeProjectsSection/HomeProjectsSection.js
@@ -1,19 +1,49 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import {Link} from "gatsby";
 import {GatsbyImage, getImage} from "gatsby-plugin-image";
 import HomeProjectItem from "./HomeProjectItem.js";
 import {FaGithub} from "@react-icons/all-files/fa/FaGithub";
 import {FaLink} from "@react-icons/all-files/fa/FaLink";
 import kMeans from "../../images/projects/k_means_visualizer.gif";
+import findYourZen from "../../images/projects/find-your-zen.gif";
 import * as styles from "./HomeProjectsSection.module.css";
 
 const HomeProjectsSection = ({projectsData}) => {
     const [active, setActive] = useState(0);
-    const GIF_PROJECTS = {"k-Means Visualizer" : kMeans};
+    const [size, setSize] = useState(window.innerWidth);
+    const GIF_PROJECTS = {"k-Means Visualizer" : kMeans,
+                          "Find Your Zen" : findYourZen};
+
+    useEffect(() => {
+        const handleResize = () => {
+            setSize(window.innerWidth);
+        }
+        window.addEventListener("resize", handleResize);
+
+        return () => {
+            window.removeEventListener("resize", handleResize);
+        }
+    })
+
+    const getNumDisplayed = () => {
+        console.log(size);
+        if (0 <= size && size <= 920) {
+            return 3;
+        }
+        else if (920 < size && size <= 1440) {
+            return 4;
+        }
+        else if (1440 < size && size < 1920) {
+            return 5;
+        }
+        else {
+            return 6;
+        }
+    }
+
     const renderShowcase = () => {
         const project = projectsData[active].node;
         const image = getImage(project.image);
-        console.log(project.image);
         return (
             <div className={styles.showcaseContainer}>
                 <h1 className={styles.showcaseTitle}>
@@ -52,7 +82,8 @@ const HomeProjectsSection = ({projectsData}) => {
         <div className={styles.container}>
             <h1 className={styles.title}>Projects</h1>
             <div className={styles.projectsContainer}>
-                {projectsData.slice(0, 4).map((data, id) => {
+                {console.log(getNumDisplayed(size))}
+                {projectsData.slice(0, getNumDisplayed()).map((data, id) => {
                     const project = data.node;
                     return (
                         <HomeProjectItem 
